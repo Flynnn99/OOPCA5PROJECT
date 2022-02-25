23e19e57a50c3d6b40e5ca7ecccfde67e1b20d9d
Flynnn99 <micflynn99@gmail.com>
2022-02-25 11:46:04 +0000

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Michael Flynn
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My Name is Michael!" );
+    }
+}
